65184b96afeed2d206ef6bad8a2a03df1f4a5bd2
1055806 <94760194+1055806@users.noreply.github.com>
2022-06-16 11:41:59 -0400

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -26,13 +26,12 @@ function logout() {
           window.location = "index.html";
       }
 
-      function send()
-      {
-            msg = document.getElementById("msg").value;
-            firebase.database().ref(room_name).push({
-                  name:user_name,
-                  message:msg,
-                  like:0
-            });
-            document.getElementById("msg").value="";
-      }
+      function send(){
+    msg=document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value="";
+}
